12b3bb07f395d12fe9b75b1075ca33924b417d7e
Tsolmon <suyocourse@gmail.com>
2026-08-08 14:03:58 +0900

    changed consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Бурзай-Зар Блог';
+export const SITE_DESCRIPTION = 'Танд хэрэгтэй зарлал, мэдээлэл, зөвлөгөө';
